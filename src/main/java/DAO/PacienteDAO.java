f378b8638a3223fbaf07263a595dab90a6e4ec1b
Kevin380897 <marden.diorge@gmail.com>
2025-08-18 23:38:31 -0300

    Commit Main

--- src/main/java/DAO/PacienteDAO.java
+++ src/main/java/DAO/PacienteDAO.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 public class PacienteDAO extends GenericDAO{
     
-    
     public List<Paciente> getAllPaciente() throws SQLException 
     {
         List<Paciente> paciente = new LinkedList<>();
